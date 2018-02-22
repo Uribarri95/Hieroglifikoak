5207046ff9e49ef7534296eb2ac80ea1315e4707
Ander <uribarri95@gmail.com>
2018-02-22 22:48:05 +0100

    mugimendu prototipoa
    salto eta aldapetan ez dabil ondo, bektoreren bat gaizki inplementaturik dago

--- Hieroglifikoak_Unity/Assets/Scripts/mugitu.cs
+++ Hieroglifikoak_Unity/Assets/Scripts/mugitu.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mugitu : MonoBehaviour {
+public class mugitu : GorputzFisikak {
 
-    public float abiaduraMax = 7f;
-    public bool lurrean;
     public float saltoIndarra = 7f;
-    private Vector2 abiadura;
+    public float abiaduraMax = 7f;
 
 	// Use this for initialization
 	void Start ()
@@ -16,11 +14,22 @@ public class mugitu : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	protected override void abiaduraKalkulatu()
     {
-        Vector2 mugitu;
+        Vector2 mugitu = Vector2.zero;
         mugitu.x = Input.GetAxis("Horizontal");
-
-
-	}
+        if (Input.GetButtonDown("Jump") && lurrean)
+        {
+            abiadura.y = 0;
+            abiadura.y = saltoIndarra;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (abiadura.y > 0)
+            {
+                abiadura.y = abiadura.y * .5f;
+            }
+        }
+        helmugaAbiadura = mugitu * abiaduraMax;
+    }
 }
