fecfeed58f346499b8a83d9cd18ae62214d3d2df
Ander <uribarri95@gmail.com>
2018-12-18 14:08:53 +0100

    irudi berriak, kakalardo eta eskorpioi etsaiak prestatu
    irudi berriak gehitu dira
    kakalardoa eta eskorpioia prest daude, IA egitea falta da

---
+++ Hieroglifikoak_Unity/Assets/Scripts/Jaurtigaia.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jaurtigaia : MonoBehaviour {
+
+    public GameObject destroyParticle;
+
+    Rigidbody2D rb;
+
+    public float desagertuDenbora;
+    public float abiadura;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * -1 * abiadura;
+        Destroy(gameObject, desagertuDenbora);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("Jokalaria kolpatuta, mina eman");
+            bool eskuma = transform.position.x > collision.transform.position.x ? true : false;
+            collision.transform.GetComponent<Eraso>().KolpeaJaso(eskuma);
+            Destroy(gameObject);
+            Instantiate(destroyParticle, transform.position, transform.rotation);
+        }
+    }
+}
