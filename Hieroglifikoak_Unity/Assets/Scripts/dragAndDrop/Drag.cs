4c885723a2d12c0a53e9f39ea1d6141919dd2eec
Ander <uribarri95@gmail.com>
2019-01-12 19:13:42 +0100

    Drag&Drop sistema
    Elementuak drag & drop daukate, piezak nahi dogun tokian utzi dezakegu eta drop egingo den tokian espazio bat sortzen da

---
+++ Hieroglifikoak_Unity/Assets/Scripts/dragAndDrop/Drag.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+
+    //public enum PiezaMota { Eragiketa, Baldintza } // baldintza -> pieza bakarra
+    //public PiezaMota mota;
+
+    GameObject piezaTokia;
+    Transform gurasoa;
+    Transform piezaGurasoa;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        piezaTokia = new GameObject();
+        piezaTokia.transform.SetParent(transform.parent);
+
+        LayoutElement layouta = piezaTokia.AddComponent<LayoutElement>();
+        layouta.preferredWidth = GetComponent<LayoutElement>().preferredWidth;
+        layouta.preferredHeight = GetComponent<LayoutElement>().preferredHeight;
+        layouta.flexibleWidth = 0;
+        layouta.flexibleHeight = 0;
+
+        piezaTokia.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+        gurasoa = transform.parent;
+        piezaGurasoa = transform.parent;
+        transform.SetParent(gurasoa.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+
+        int piezaPos = piezaGurasoa.childCount;
+        for (int i = 0; i < piezaGurasoa.childCount; i++)
+        {
+            if(transform.position.y > piezaGurasoa.GetChild(i).position.y)
+            {
+                piezaPos = i;
+                if(piezaTokia.transform.GetSiblingIndex() < piezaPos)
+                {
+                    piezaPos--;
+                }
+                break;
+            }
+        }
+        piezaTokia.transform.SetSiblingIndex(piezaPos);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(gurasoa);
+        transform.SetSiblingIndex(piezaTokia.transform.GetSiblingIndex());
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(piezaTokia);
+    }
+
+    public void PiezaEzabatu()
+    {
+        Destroy(piezaTokia);
+    }
+
+    public Transform GetGurasoa()
+    {
+        return gurasoa;
+    }
+
+    public void SetGurasoa(Transform guraso)
+    {
+        gurasoa = guraso;
+    }
+
+    public Transform GetPiezaGurasoa()
+    {
+        return piezaGurasoa;
+    }
+
+    public void SetPiezaGurasoa(Transform guraso)
+    {
+        piezaGurasoa = guraso;
+    }
+
+    public void SetPiezaTokiGurasoa(Transform guraso)
+    {
+        piezaTokia.transform.SetParent(guraso);
+    }
+}
---
+++ Hieroglifikoak_Unity/Assets/Scripts/dragAndDrop/DropTokia.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropTokia : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    //public Drag.PiezaMota mota; // baldintza -> pieza bakarra
+    RectTransform tamaina;
+    public float luzera;
+    public float piezaAltuera;
+    public float altueraMin = 100;
+    public float padding = 20;
+    public float spacing = 2;
+    float altuera;
+    float semeKop;
+
+    // Use this for initialization
+    void Start()
+    {
+        semeKop = transform.childCount;
+        altuera = padding + (piezaAltuera * semeKop) + (spacing * (semeKop - 1));
+        if(altuera < altueraMin)
+        {
+            altuera = altueraMin;
+        }
+        tamaina = GetComponent<RectTransform>();
+        tamaina.sizeDelta = new Vector2(luzera, altuera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(semeKop < transform.childCount)
+        {
+            semeKop = transform.childCount;
+            altuera = padding + (piezaAltuera * semeKop) + (spacing * (semeKop - 1));
+        }
+        tamaina.sizeDelta = new Vector2(luzera, altuera);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Drag pieza = eventData.pointerDrag.GetComponent<Drag>();
+        if(pieza != null)
+        {
+            // if pieza.mota == mota
+            pieza.SetGurasoa(transform);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Drag pieza = eventData.pointerDrag.GetComponent<Drag>();
+        if(pieza!= null)
+        {
+            pieza.SetPiezaGurasoa(transform);
+            pieza.SetPiezaTokiGurasoa(transform);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Drag pieza = eventData.pointerDrag.GetComponent<Drag>();
+        pieza.SetPiezaTokiGurasoa(pieza.GetGurasoa().parent);
+
+        if (pieza != null && pieza.GetGurasoa() == transform)
+        {
+            pieza.SetPiezaGurasoa(transform);
+        }
+    }
+}
